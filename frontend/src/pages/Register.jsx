3cc34c53d00a9574f8c38ff583c528177101c13f
2403717620521026-gif <2403717620521026@cit.edu.in>
2026-06-18 02:44:44 +0530

    Use env API base URL everywhere

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 // src/pages/Register.jsx
 import { useState } from "react";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function Register({ switchToLogin, onRegister }) {
   const [form, setForm] = useState({
